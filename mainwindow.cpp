ac31af4109a064dc4ee9afb218b766a80edb5308
Patryk Chrabaszcz <Haskis00@gmail.com>
2013-12-01 17:19:29 +0100

    Patryk-2

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -5,20 +5,36 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow),
     stereoVision(new StereoVision)
+
 {
     ui->setupUi(this);
 
+
+    stereoVision->setPatternSize(9,6);
+    Mat leftImage;
+    Mat rightImage;
     //Just for check
+    QString url= "/home/haskis/Pictures/Calibration/";
+    for(int i=1; i<6;i++){
+        QString left=url+"left0"+QString::number(i)+".jpg";
+        QString right=url+"right0"+QString::number(i)+".jpg";
+        String leftS=left.toStdString();
+        String rightS=right.toStdString();
+        qDebug()<<left<<right;
 
-    Mat leftImage = imread("/home/haskis/Pictures/leftImage.jpg",CV_LOAD_IMAGE_GRAYSCALE);
+        leftImage = imread(leftS,CV_LOAD_IMAGE_GRAYSCALE);
 
-    Mat rightImage = imread("/home/haskis/Pictures/rightImage.jpg",CV_LOAD_IMAGE_GRAYSCALE);
+        rightImage = imread(rightS,CV_LOAD_IMAGE_GRAYSCALE);
 
-    stereoVision->changePatternSize(9,6);
-    qDebug()<<stereoVision->addSampleToCalibration(leftImage,rightImage);
+        stereoVision->addSampleToCalibration(leftImage,rightImage);
+    }
+    stereoVision->setImageSize(leftImage.size());
+    stereoVision->calibrateStereoCameras();
 
-    imshow("a", leftImage);
-    imshow("b", rightImage);
+    drawChessboardCorners(leftImage,Size(9,6), stereoVision->imagePoints[0][4], true);
+    drawChessboardCorners(rightImage,Size(9,6), stereoVision->imagePoints[1][4], true);
+    imshow("a",rightImage);
+    imshow("B",leftImage);
 
 }
 
--- stereovision.h
+++ stereovision.h
@@ -5,6 +5,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include <QDebug>
 using namespace cv;
 
 class StereoVision
@@ -13,20 +14,27 @@ class StereoVision
 public:
     StereoVision();
     bool addSampleToCalibration(Mat &leftImage, Mat &rightImage);
+    bool calibrateStereoCameras();
 
-    void changePatternSize(Size x){ patternSize =x;}
-    void changePatternSize(int x, int y){ patternSize = Size(x,y);}
+    void setPatternSize(Size x){ patternSize =x;}
+    void setPatternSize(int x, int y){ patternSize = Size(x,y);}
+    void setImageSize(Size x) { imageSize= x;}
+
+    vector<vector<Point2f> > imagePoints[2];
+    vector<vector<Point3f> > objectPoints;
 private:
 
     //Calibration Begin
 
-    int addedCalibrationSamplesCounter;
-    int acceptedCalibrationSamplesCounter;
-
-    vector<vector<Point2f> > imagePoints[2];
-    vector<vector<Point3f> > objectPoints;
+    int samplesCounter;
+    int goodSamplesCounter;
+    int minSamples;
+    float squareSize; //size of calibration pattern
 
     Size patternSize; //Number of inner corners per a chessboard row and column for (8x8) board it's (7x7)
+    Size imageSize;
+    Mat cameraMatrix[2], distCoeffs[2];
+    Mat R, T, E, F;
 
 };
 
